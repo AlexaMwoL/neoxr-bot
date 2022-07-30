2d351aafe207fdbeab5e5623938765ca0da04e39
TEAM ALEXA <93711729+AlexaMwoL@users.noreply.github.com>
2022-07-30 13:25:09 +0530

    Update config.js

--- system/config.js
+++ system/config.js
@@ -15,28 +15,28 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6285221100126@c.us'
+global.forwards = '919633687665@c.us'
 // Get neoxr apikey by registering at https://api.nxr.my.id
 global.Api = new (require('./neoxrApi'))('2lleJx')
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
 global.chatai_bid = '164728'
 global.chatai_key = 'MKPsfkgXLZPGrWoH'
 // Footer text
-global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
+global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ  ᴋʀɪꜱʜɴᴀᴅᴀꜱ 🎈ッ'
 // Global status
 global.status = Object.freeze({
    wait: Func.texted('bold', 'Processed . . .'),
-   invalid: Func.texted('bold', 'URL is Invalid!'),
-   wrong: Func.texted('bold', 'Wrong format!'),
-   getdata: Func.texted('bold', 'Scraping metadata . . .'),
-   fail: Func.texted('bold', 'Can\'t get metadata!'),
-   error: Func.texted('bold', 'Error occurred!'),
-   errorF: Func.texted('bold', 'Sorry this feature is in error.'),
-   premium: Func.texted('bold', 'This feature only for premium user.'),
-   owner: Func.texted('bold', 'This command only for owner.'),
-   god: Func.texted('bold', 'This command only for Master'),
-   group: Func.texted('bold', 'This command will only work in groups.'),
-   botAdmin: Func.texted('bold', 'This command will work when I become an admin.'),
-   admin: Func.texted('bold', 'This command only for group admin.'),
-   private: Func.texted('bold', 'Use this command in private chat.')
+   invalid: Func.texted('bold', '*URL is Invalid!*'),
+   wrong: Func.texted('bold', '*Wrong format!*'),
+   getdata: Func.texted('bold', '*Scraping metadata . . .*'),
+   fail: Func.texted('bold', '*Can\'t get metadata!*'),
+   error: Func.texted('bold', '*Error occurred!*'),
+   errorF: Func.texted('bold', '*Sorry this feature is in error.*'),
+   premium: Func.texted('bold', '*This feature only for premium user.*'),
+   owner: Func.texted('bold', '*This command only for owner.*'),
+   god: Func.texted('bold', '*This command only for Master*'),
+   group: Func.texted('bold', '*This command will only work in groups.*'),
+   botAdmin: Func.texted('bold', '*This command will work when I become an admin.*'),
+   admin: Func.texted('bold', '*This command only for group admin.*'),
+   private: Func.texted('bold', '*Use this command in private chat.*')
 })
